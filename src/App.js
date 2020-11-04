6503eef0e1f2b18718bb2e8fb49c2b33c398a3b6
Yannistan <tanis.ioannis@gmail.com>
2020-11-04 10:41:50 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Heading, Text, Center, Button } from '@chakra-ui/core'
+import {ethers} from 'ethers'
+
+function App() {
+  return <>
+  <Center>
+    <Heading>Web3 demo 1</Heading>
+  </Center>
+  </>
+}
+
+export default App
